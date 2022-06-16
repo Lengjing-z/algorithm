ec8194b723b4f4be735445e86cf4ab1f5a285a2b
zhangnian <xxxx@163.com>
2022-06-16 13:52:33 +0800

    测试

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author zhangnian
+ * @title: Test
+ * @projectName Algorithm
+ * @description: TODO
+ * @date 2022/6/16  13:52:14
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
